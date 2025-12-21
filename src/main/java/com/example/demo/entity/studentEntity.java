ff0e413e2cf660e6de05068489f0c755c26b29f0
bala-amypo <bala@amypo.in>
2025-12-21 18:44:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -1,35 +1,81 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
-public class studentEntity{
+@Table(name = "student")
+public class Studententity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @NotBlank
+    @NotBlank(message = "name cannot be blank")
+    @Column(unique = true, nullable = false)
     private String name;
 
-    @Email(message="Name is not Valid")
+    @NotBlank(message = "email cannot be blank")
+    @Email(message = "invalid format")
+    @Column(unique = true, nullable = false)
     private String email;
 
-    public void setId(long id){
-        this.id=id;
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+   
+    public Studententity() {
     }
-    public long getId(){
+
+    
+    public Studententity(Long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public studentEntity(){
-
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public studentEntity(long id,String name,String email){
-        this.id=id;
-        this.name=name;
-        this.email=email;
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
